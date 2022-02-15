99e94e998fa12e8bfe29151294fa681af0003d0c
temikfart <fartygin.a@phystech.edu>
2022-02-15 19:39:25 +0300

    w1(int_types): complete task 1.

---
+++ week-1/int_types/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  if (N <= 0) {
+    cout << 0 << endl;
+    return 0;
+  }
+  
+  vector<int> t(N);
+  int64_t sum = 0;
+  for (int i = 0; i < N; i++) {
+    cin >> t[i];
+    sum += t[i];
+  }
+  
+  int avg;
+  avg = static_cast<int>(sum / N);
+  
+  vector<int> v;
+  for (int i = 0; i < N; i++) {
+    if (t[i] > avg) {
+      v.push_back(i);
+    }
+  }
+  int k = static_cast<int>(v.size());
+  
+  cout << k << endl;
+  for (int i = 0; i < k; i++) {
+    if (i > 0) {
+      cout << " ";
+    }
+    cout << v[i];
+  }
+}
